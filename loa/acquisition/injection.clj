e80d9bb1b235c56354c6b2242a3d2295730c820b
unknown <karl.k.magnusson@gmail.com>
2010-10-14 19:33:29 +0200

    Added set-rarity to dreamcast cards. Changed back Elsp v Tezz code.

--- loa/acquisition/injection.clj
+++ loa/acquisition/injection.clj
@@ -63,70 +63,72 @@ gatherer.")
 ;;  Dreamcast
 ;;
 (def dreamcast-cards
-     [{:name "Arden Angel"
-       :cost "4WW"
-       :types ["Creature" "Angel"]
-       :pow "4"
-       :tgh "4"
-       :rules [{:text "Flying"}
-               {:text "At the beginning of your upkeep, if Arden Angel is in your graveyard, choose a number from 1 to 4 at random. If it's 1, you may return Arden Angel onto the battlefield."}]}
+     (map
+      #(assoc %1 :set-rarity [["Dreamcast" :special]])
+      [{:name "Arden Angel"
+        :cost "4WW"
+        :types ["Creature" "Angel"]
+        :pow "4"
+        :tgh "4"
+        :rules [{:text "Flying"}
+                {:text "At the beginning of your upkeep, if Arden Angel is in your graveyard, choose a number from 1 to 4 at random. If it's 1, you may return Arden Angel onto the battlefield."}]}
 
-      {:name "Ashuza's Breath"
-       :cost "1R"
-       :types ["Sorcery"]
-       :rules [{:text "Ashuza's Breath deals X damage to each creature, where X is a number chosen at random from 0 to 2."}]}
+       {:name "Ashuza's Breath"
+        :cost "1R"
+        :types ["Sorcery"]
+        :rules [{:text "Ashuza's Breath deals X damage to each creature, where X is a number chosen at random from 0 to 2."}]}
 
-      {:name "Camato Scout"
-       :cost "1UU"
-       :types ["Creature" "Merfolk" "Scout"]
-       :pow "2"
-       :tgh "3"
-       :rules [{:text "When Camato Scout enters the battlefield, it gains landwalk of a basic land type chosen at random. (This effect lasts indefinitely.)"}]}
+       {:name "Camato Scout"
+        :cost "1UU"
+        :types ["Creature" "Merfolk" "Scout"]
+        :pow "2"
+        :tgh "3"
+        :rules [{:text "When Camato Scout enters the battlefield, it gains landwalk of a basic land type chosen at random. (This effect lasts indefinitely.)"}]}
 
-      {:name "Hapato's Might"
-       :cost "2B"
-       :types ["Instant"]
-       :rules [{:text "Target creature gets +X/+0 until end of turn, where X is a number chosen randomly from 0 to 6."}]}
+       {:name "Hapato's Might"
+        :cost "2B"
+        :types ["Instant"]
+        :rules [{:text "Target creature gets +X/+0 until end of turn, where X is a number chosen randomly from 0 to 6."}]}
 
-      {:name "Lydari Druid"
-       :cost "2G"
-       :types ["Creature" "Human" "Druid"]
-       :pow "2"
-       :tgh "2"
-       :rules [{:text "When Lydari Druid enters the battlefield, for each land, choose a basic land type at random. That land becomes that land type." :reminder "This effect lasts indefinitely."}]}
+       {:name "Lydari Druid"
+        :cost "2G"
+        :types ["Creature" "Human" "Druid"]
+        :pow "2"
+        :tgh "2"
+        :rules [{:text "When Lydari Druid enters the battlefield, for each land, choose a basic land type at random. That land becomes that land type." :reminder "This effect lasts indefinitely."}]}
 
-      {:name "Lydari Elephant"
-       :cost "4G"
-       :types ["Creature" "Elephant"]
-       :pow "*"
-       :tgh "*"
-       :rules [{:text "Lydari Elephant enters the battlefield as a X/Y creature, where X and Y are numbers randomly chosen from 3 to 7."}]}
+       {:name "Lydari Elephant"
+        :cost "4G"
+        :types ["Creature" "Elephant"]
+        :pow "*"
+        :tgh "*"
+        :rules [{:text "Lydari Elephant enters the battlefield as a X/Y creature, where X and Y are numbers randomly chosen from 3 to 7."}]}
 
-      {:name "Murgish Cemetery"
-       :cost "4BB"
-       :types ["Enchantment"]
-       :rules [{:text "3B, Discard a card: Put an X/X black Zombie token onto the battlefield, where X is a number randomly chosen from 2 to 6."}]}
+       {:name "Murgish Cemetery"
+        :cost "4BB"
+        :types ["Enchantment"]
+        :rules [{:text "3B, Discard a card: Put an X/X black Zombie token onto the battlefield, where X is a number randomly chosen from 2 to 6."}]}
 
-      {:name "Saji's Torrent"
-       :cost "1U"
-       :types ["Instant"]
-       :rules [{:text "Choose a number from 0 to 5 at random, then choose that many creatures and tap them."}]}
+       {:name "Saji's Torrent"
+        :cost "1U"
+        :types ["Instant"]
+        :rules [{:text "Choose a number from 0 to 5 at random, then choose that many creatures and tap them."}]}
 
-      {:name "Tornellan Protector"
-       :cost "2W"
-       :types ["Creature" "Human" "Cleric"]
-       :pow "1"
-       :tgh "2"
-       :rules [{:text "T: Choose a number from 1 to 3 at random. Until end of turn, if a source would deal damage to target creature or player, it deals that much damage minus the chosen number to that creature or player instead."}]}
+       {:name "Tornellan Protector"
+        :cost "2W"
+        :types ["Creature" "Human" "Cleric"]
+        :pow "1"
+        :tgh "2"
+        :rules [{:text "T: Choose a number from 1 to 3 at random. Until end of turn, if a source would deal damage to target creature or player, it deals that much damage minus the chosen number to that creature or player instead."}]}
 
-      {:name "Velican Dragon"
-       :cost "5RR"
-       :types ["Creature" "Dragon"]
-       :pow "5"
-       :tgh "5"
-       :rules [{:text "Flying"}
-               {:text "Whenever Velican Dragon becomes blocked, it gets +X/+0 until end of turn, where X is a number chosen at random from 0 to 5."}]}
-      ])
+       {:name "Velican Dragon"
+        :cost "5RR"
+        :types ["Creature" "Dragon"]
+        :pow "5"
+        :tgh "5"
+        :rules [{:text "Flying"}
+                {:text "Whenever Velican Dragon becomes blocked, it gets +X/+0 until end of turn, where X is a number chosen at random from 0 to 5."}]}
+       ]))
 
 ;;-------------------------------------------------
 ;;
