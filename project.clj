8e18ce54e561acff020d6ad1c8b477752bf4178a
Karl Magnusson <karl.k.magnusson@gmail.com>
2010-11-29 21:56:02 +0100

    Moved to lein.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject loa "1.0.0-SNAPSHOT"
+  :description "A crawler/parser for wizards of the coasts gatherer."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-run "1.0.0"]
+                     [swank-clojure "1.2.1"]])
