8d201cf6a64e9d52d9d02ad85733fb2386c3fa8a
Karl Magnusson <karl.k.magnusson@gmail.com>
2012-07-25 21:36:38 +0200

    Revert "Removed duplicates from text-data caused by multiple pieces of art in the same set."
    This reverts commit 8d75dd7650df2ce952039f086512f177a503175b.

--- src/loa/format/card_text.clj
+++ src/loa/format/card_text.clj
@@ -135,8 +135,6 @@
 (defn- from-meta
   [meta set-map]
   (let [meta (->> meta
-                  (map #(select-keys % [:set :code :rarity :number]))
-                  set
                   (sort-by #(-> % :set set-map :release-date))
                   (map (fn [{:keys [set rarity]}]
                          (str (-> set set-map :code) " "
